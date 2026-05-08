59e5fb22bc3869ffb910ea3ea7f64fe3838727c1
futshashsc-debug <futshashsc@gmail.com>
2026-05-08 19:36:21 +0600

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            s[l]=s[l]^s[r];
+            s[r]=s[l]^s[r];
+             s[l]=s[l]^s[r];
+             l++;r--;
+        }
+        return;
+
+    }
+};
